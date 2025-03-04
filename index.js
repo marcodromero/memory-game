2155349d1aff2e22ae668ca35c2fad809c1af38d
Marco Romero <marcod.romero@gmail.com>
2025-03-04 19:14:28 -0300

    Registro de datos de la tabla de clasificación y persistencia de datos en el dispositivo

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 class App{
   constructor(){
     this.emojis = ["🐬","🍕","🎁","🤪","🦁","🔒", "🌻","🥳","🎹", "🐬","🍕","🎁","🤪","🦁","🔒", "🌻","🥳","🎹"];
-    this.btnStartGame = document.getElementById("btnStartGame");
     this.board = document.getElementById("board");
     this.results = document.getElementById("results");
+    this.leaderboard= document.getElementById("leaderboard");
+    this.menu = document.getElementById("menu");
+    this.btnStartGame = document.getElementById("btnStartGame");
+    this.btnStartGame2 = document.getElementById("btnStartGame2");
+    this.btnSaveResult = document.getElementById("btnSaveResult");
+    this.btnLeaderboard = document.getElementById("btnLeaderboard");
+    this.btnDeleteLeaderboard = document.getElementById("btnDeleteLeaderboard");
+    this.btnRules= document.getElementById("btnRules");
     this.elections = [];
     this.isGameCompleted = false;
-    this.menu = document.getElementById("menu");
     this.counter = 0;
     this.seconds = 0;
     this.minutes = 0
     this.intervalId = null;
+    this.dataLeaderboard = [];
   }
 
   init(){
-    this.btnStartGame.addEventListener("click", this.startGame());
+    let dataBrowserLeaderboard = localStorage.getItem("leaderboard");
+    (dataBrowserLeaderboard != undefined) && (this.dataLeaderboard = JSON.parse(dataBrowserLeaderboard));
+    this.btnStartGame.addEventListener("click", ()=>this.startGame());
+    this.btnStartGame2.addEventListener("click", ()=>this.startGame());
+    this.btnSaveResult.addEventListener("click", ()=>this.saveResult());
+    this.btnLeaderboard.addEventListener("click", ()=>this.renderLeaderboard());
+    this.btnDeleteLeaderboard.addEventListener("click", ()=>this.deleteLeaderboard());
+    this.btnRules.addEventListener("click", ()=>this.renderMainMenu());
+    this.renderMainMenu();
   } 
 
   startGame(){
@@ -27,7 +42,6 @@ class App{
   }
 
   startChronometer(){
-    console.log("iniciado chronometro funcion");
     this.intervalId = setInterval(() => {
       console.log(this.minutes, this.seconds);
       if(this.seconds == 59){
@@ -39,9 +53,18 @@ class App{
     }, 1000);
   }
 
+  renderMainMenu(){
+    this.menu.style.display = "block";
+    this.leaderboard.style.display = "none";
+    if(this.dataLeaderboard.length == 0){
+      this.btnLeaderboard.style.display = "none";
+    }
+  }
+
   renderBoard(){
     this.menu.style.display = "none";
-    this.board.style.display = "grid";   
+    this.leaderboard.style.display = "none";   
+    this.board.style.display = "grid";    
     
     for(let i = 0; i < this.emojis.length ; i++){
       let flipCard = document.createElement("div");
@@ -112,13 +135,13 @@ class App{
     if(this.counter == (this.emojis.length)/2){
           clearInterval(this.intervalId);
           console.log(this.counter, (this.emojis.length)/2)
-          this.renderResult();
+          this.renderResult(); 
       }
   }
 
   blockCards(){
-    const card1= document.getElementById(String(this.elections[0]));
-    const card2= document.getElementById(String(this.elections[1]));
+    const card1 = document.getElementById(String(this.elections[0]));
+    const card2 = document.getElementById(String(this.elections[1]));
     card1.removeEventListener("click", this.saveElectionOfPlayer);
     card2.removeEventListener("click", this.saveElectionOfPlayer);
   }
@@ -131,10 +154,58 @@ class App{
     this.results.style.display = "block";
   }
 
+  renderLeaderboard(){
+    const leaderboardTbody = document.getElementById("leaderboardTbody");
+    leaderboardTbody.innerHTML = '';
+    this.dataLeaderboard.forEach((element, index)=>{
+      const tr = document.createElement("tr");
+      const tdTextName = document.createElement("td");
+      const tdTextTime = document.createElement("td");
+      const tdTextPosition = document.createElement("td");
+      const textName = document.createTextNode(element.name);
+      const textTime = document.createTextNode(`${element.minutes}:${(element.seconds).toString().padStart(2,"0")}`);
+      const textPosition = document.createTextNode(index + 1);
+      tdTextPosition.appendChild(textPosition);
+      tdTextName.appendChild(textName);
+      tdTextTime.appendChild(textTime);
+      tr.appendChild(tdTextPosition);
+      tr.appendChild(tdTextName);
+      tr.appendChild(tdTextTime);
+      leaderboardTbody.appendChild(tr);
+    })
+
+    this.menu.style.display = "none";
+    this.results.style.display = "none";
+    this.leaderboard.style.display = "block";
+  }
+
   saveResult(){
     let playerName = document.getElementById("playerName").value;
-
+    console.log("playerName", playerName);
+    const playerData = {name: playerName, minutes: this.minutes, seconds: this.seconds};
+    this.dataLeaderboard.push(playerData);
+    this.dataLeaderboard = this.sortLeaderboard();
+    console.log("dataLeaderboardSorted", this.dataLeaderboard);
+    localStorage.setItem("leaderboard", JSON.stringify(this.dataLeaderboard));
+    this.renderLeaderboard();
   }
+
+
+  sortLeaderboard(){
+    let arraySorted = this.dataLeaderboard.sort((a, b) => {
+      if(a.minutes == b.minutes){
+        return a.seconds - b.seconds;
+      }
+      return a.minutes - b.minutes;
+    });
+    return arraySorted;
+  }
+
+  deleteLeaderboard(){
+    localStorage.removeItem("leaderboard");
+    this.dataLeaderboard = [];
+  }
+
 }
 
 const app = new App();
